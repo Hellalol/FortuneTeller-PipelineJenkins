efbb592c58ce4c2816779229aa86b3f3f471f01b
Fredrik Billing <frhabi@gmail.com>
2020-09-06 20:08:39 +0200

    Coverage restcontroller

--- src/test/java/com/example/fortuneteller/controller/FortuneControllerRestTest.java
+++ src/test/java/com/example/fortuneteller/controller/FortuneControllerRestTest.java
@@ -1,5 +1,24 @@
 package com.example.fortuneteller.controller;
 
+import com.example.fortuneteller.domain.Person;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Map;
+
+@SpringBootTest
 public class FortuneControllerRestTest {
 
+    @Autowired
+    FortuneControllerRest fortuneControllerRest;
+
+    @Test
+    void submitInfoTest() {
+        Person person1 = new Person("Fredrik","Svensk",29,"male");
+        Map<String,String> info = fortuneControllerRest.submitInfo(person1.getName(),person1.getAge(),person1.getNationality(),person1.getGender());
+        System.out.println();
+        Assertions.assertTrue(info.get("message").startsWith("Your future will be Busy"));
+    }
 }
